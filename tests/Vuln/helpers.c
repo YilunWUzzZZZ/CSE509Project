0374bfabd2b961ff42dde9f96a32eaf4be54836c
sgoutam <8445239+sgoutam@users.noreply.github.com>
2021-12-19 14:49:50 -0500

    added build script

--- tests/Vuln/helpers.c
+++ tests/Vuln/helpers.c
@@ -2,15 +2,17 @@
 
 int find_in(char *my_str, char *string_list[], size_t num_strings)
 {
+  
     for ( int i = 0; i < num_strings; i++ )
         if (strcmp(my_str, string_list[i]) == 0 )
             return 1;
-
+    
     return 0;
 }
 
 int safetoread_path(char *pathname)
 {
+  
   char *file_list[] = { "vuln", "ld.so.preload", "libc.so.6" ,"ld.so.cache" };
   char *filename = basename(pathname);
   
