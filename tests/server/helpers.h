07c1342538a52a7678ca9ea0c3087037ad3c7daa
maryros67 <95501530+maryros67@users.noreply.github.com>
2021-12-19 10:17:39 +0330

    Update helpers.h

--- tests/server/helpers.h
+++ tests/server/helpers.h
@@ -6,6 +6,6 @@
 
 int find_in(char *my_str, char *string_list[], size_t num_strings);
 int safetoread_path(char *pathname);
-int safetoread_addr(struct sockaddr_in *);
+int safetoread_addr(struct sockaddr_in *addr);
 
 
